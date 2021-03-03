a358e6f9b0b70dfec72d7e854510c4b5eb3d5b06
graf94 <grafov2711@gmail.com>
2021-03-03 20:03:56 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function() {
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        starWidth: "12px",
+        rating: 5,
+        readOnly: true
 
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
